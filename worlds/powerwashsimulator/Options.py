31f3967033255653d91f653a95d1c44c29db9051
SWCreeperKing <SW.CreeperKing@gmail.com>
2025-06-14 18:45:13 -0700

    v.0.2

--- worlds/powerwashsimulator/Options.py
+++ worlds/powerwashsimulator/Options.py
@@ -1,8 +1,9 @@
 import logging
-from typing import List
+from typing import List, Union
 from dataclasses import dataclass
-from Options import Range, Toggle, PerGameCommonOptions, OptionSet
+from Options import Range, Toggle, PerGameCommonOptions, OptionSet, OptionError
 from .Locations import raw_location_dict
+from settings import Group, Bool
 
 
 class StartWithVan(Toggle):
@@ -36,11 +37,35 @@ class FillerToMcGuffin(Range):
     range_end = 100
     default = 10
 
+class Sanities(OptionSet):
+    """
+    Which sanities should be enabled?
+
+    Percentsanity: every % total cleaned of a level is a check
+    Objectsanity: each part of a cleanable object is a check
+    """
+    display_name = "Sanities"
+    valid_keys = frozenset(["Percentsanity", "Objectsanity"])
+    default = "Percentsanity"
+
+class Percentsanity(Range):
+    """
+    What intervals of cleaned % to have checks at
+
+    default 20: checks at 20%, 40%, 60%, 80%, and 100%
+    host yaml setting `allow_percentsanity_below_7` (off by default) will allow % below 7%
+    """
+    default = 20
+    range_end = 20
+    range_start = 1
+
 @dataclass
 class PowerwashSimulatorOptions(PerGameCommonOptions):
     start_with_van: StartWithVan
     locations: Locations
     filler_to_mcguffin: FillerToMcGuffin
+    sanities: Sanities
+    percentsanity: Percentsanity
 
     def get_locations(self) -> List[str]:
         locations = [loc for loc in self.locations]
@@ -50,9 +75,31 @@ class PowerwashSimulatorOptions(PerGameCommonOptions):
 
         return locations
 
+class PowerwashSimulatorSettings(Group):
+    class AllowPercentsanityBelow7(Bool):
+        """Allow players to have the Percentsanity setting to be below 7%"""
+
+    class AllowObjectsanity(Bool):
+        """Allow players to enable Objectsanity"""
+
+    allow_percentsanity_below_7: Union[AllowPercentsanityBelow7, bool] = False
+    allow_objectsanity: Union[AllowObjectsanity, bool] = False
+
 def check_options(world):
     options: PowerwashSimulatorOptions = world.options
+    settings: PowerwashSimulatorSettings = world.settings
+
+    if options.percentsanity < 7 and not settings.allow_percentsanity_below_7:
+        logging.info(
+            f"Powerwash Simulator: {world.player_name} has {settings.allow_percentsanity_below_7} < 7. since the host has allow_percentsanity_below_7 false percentsanity will be set to 7")
+        options.percentsanity = Percentsanity(7)
+
+    if "Objectsanity" in options.sanities and not settings.allow_objectsanity:
+        RaiseYamlError(world.player_name, "Objectsanity can not be enabled unless the host setting 'allow_objectsanity' is also enabled")
 
     if len(options.get_locations()) > 0: return
-    logging.error(
-        f"Powerwash Simulator: {world.player_name} Does not have locations listed in their yaml, PLEASE FIX YOUR YAML")
+    RaiseYamlError(world.player_name, "Does not have locations listed in their yaml")
+
+def RaiseYamlError(player_name, error):
+    raise OptionError(
+        f"\n\n=== Powerwash Simulator YAML ERROR ===\nPowerwash Simulator: {player_name} {error}, PLEASE FIX YOUR YAML\n\n")
--- worlds/powerwashsimulator/__init__.py
+++ worlds/powerwashsimulator/__init__.py
@@ -1,11 +1,13 @@
-from typing import Dict, Any
+import math
+from typing import Dict, Any, Union, ClassVar
 from worlds.AutoWorld import World
 from BaseClasses import Location, Region
 from .Items import raw_items, PowerwashSimulatorItem, item_table, create_items
-from .Locations import location_dict, raw_location_dict, locations_percentages, land_vehicles
-from .Options import PowerwashSimulatorOptions, check_options
+from .Locations import location_dict, raw_location_dict, locations_percentages, land_vehicles, objectsanity_dict
+from .Options import PowerwashSimulatorOptions, PowerwashSimulatorSettings, check_options
 
 uuid_offset = 0x3AF4F1BC
+
 class PowerwashSimulator(World):
     """
     Powerwash Simulator
@@ -13,10 +15,12 @@ class PowerwashSimulator(World):
     game = "Powerwash Simulator"
     options_dataclass = PowerwashSimulatorOptions
     options: PowerwashSimulatorOptions
+    settings: ClassVar[PowerwashSimulatorSettings]
     location_name_to_id = {value: location_dict.index(value) + uuid_offset for value in location_dict}
     item_name_to_id = {value: raw_items.index(value) + uuid_offset for value in raw_items}
     starting_location = land_vehicles[0]
-    location_counter = 0
+    location_counter:int = 0
+    mcguffin_requirement = 0
 
     def generate_early(self) -> None:
         check_options(self)
@@ -29,21 +33,37 @@ class PowerwashSimulator(World):
         option_locations = self.options.get_locations()
         menu_region = Region("Menu", self.player, self.multiworld)
         self.multiworld.regions.append(menu_region)
+        percentsanity = self.options.percentsanity
 
         for location in option_locations:
             next_region = Region(f"Clean the {location}", self.player, self.multiworld)
             self.multiworld.regions.append(next_region)
 
-            for percent_location in locations_percentages[location]:
+            if "Percentsanity" in self.options.sanities:
+                for i in range(percentsanity, 100, percentsanity):
+                    self.location_counter += 1
+                    percent_location = f"{location} {i}%"
+                    percentsanity_location = Location(self.player, percent_location, self.location_name_to_id[percent_location], next_region)
+                    next_region.locations.append(percentsanity_location)
+
+                percent_location = f"{location} 100%"
+                percentsanity_location = Location(self.player, percent_location, self.location_name_to_id[percent_location], next_region)
+                next_region.locations.append(percentsanity_location)
                 self.location_counter += 1
-                location_check = Location(self.player, percent_location, self.location_name_to_id[percent_location], next_region)
-                next_region.locations.append(location_check)
+
+            if "Objectsanity" in self.options.sanities:
+                for part in objectsanity_dict[location]:
+                    self.location_counter += 1
+                    objectsanity_location = Location(self.player, part, self.location_name_to_id[part], next_region)
+                    next_region.locations.append(objectsanity_location)
 
             if location == self.starting_location:
                 menu_region.connect(next_region)
             else:
                 menu_region.connect(next_region, rule=lambda state, location_lock=location: state.has(f"{location_lock} Unlock", self.player))
             next_region.connect(menu_region)
+
+        self.mcguffin_requirement = max(min(math.floor(self.location_counter * .1), self.location_counter - len(option_locations) * 2), len(option_locations))
 
     def create_item(self, name: str) -> PowerwashSimulatorItem:
         return PowerwashSimulatorItem(name, item_table[name], self.item_name_to_id[name], self.player)
@@ -58,7 +78,9 @@ class PowerwashSimulator(World):
     def fill_slot_data(self) -> Dict[str, Any]:
         slot_data: Dict[str, Any] = {
             "starting_location": str(self.starting_location),
-            "jobs_done": int(len(self.options.get_locations()))
+            "jobs_done": int(self.mcguffin_requirement),
+            "objectsanity": bool("Objectsanity" in self.options.sanities),
+            "percentsanity": bool("Percentsanity" in self.options.sanities),
         }
 
         return slot_data
