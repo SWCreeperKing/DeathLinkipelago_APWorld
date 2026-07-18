dff7e17aa44b39b33ecb5d7497e40a26d4305307
SWCreeperKing <sw.creeperking@gmail.com>
2026-07-17 17:55:00 -0700

    bridge

---
+++ worlds/HydraUTBridge/__init__.py
@@ -0,0 +1,55 @@
+import json
+
+from NetUtils import NetworkItem
+from worlds.LauncherComponents import Component, components, Type
+
+def run_tracker():
+    try:
+        import logging
+        from worlds.tracker.TrackerCore import TrackerCore
+        from worlds.AutoWorld import AutoWorldRegister
+        from worlds.tracker import DeferredEntranceMode
+
+        print("READY")
+
+        print("slot_name")
+        slot_name = input()
+
+        print("game")
+        game = input()
+
+        logger = logging.getLogger("Client")
+        tracker_core = TrackerCore(logger, False, False)
+        tracker_core.enforce_deferred_connections = DeferredEntranceMode.disabled
+
+        print("slot_data")
+        slot_data = json.loads(input())
+        tracker_core.run_generator(slot_data, None)
+
+        connected_cls = AutoWorldRegister.world_types.get(game)
+        tracker_core.set_slot_params(game, 1, slot_name, 1)
+        tracker_core.initalize_tracker_core(connected_cls, slot_data)
+
+        print("missing_locations")
+        tracker_core.set_missing_locations(set(parse_ids(input())))
+        
+        while True:
+            print("next")
+            nxt = input()
+            if nxt == "stop": return
+            split = nxt.split('|')
+
+            tracker_core.set_items_received([NetworkItem(item, -1, -1, 0) for item in parse_ids(split[1])])
+
+            print(f"Circle {split[0]}|{[tracker_core.get_current_world().location_name_to_id[loc_name] for loc_name in
+                      tracker_core.updateTracker().in_logic_locations]}")
+    except Exception as err:
+        print(f"ERROR: {err}")
+
+
+def parse_ids(locs: str):
+    if locs == "": return [];
+    return [int(item.strip()) for item in locs.split(',')]
+
+
+components.append(Component("HydraUTBridge", None, func=run_tracker, component_type=Type.CLIENT))
